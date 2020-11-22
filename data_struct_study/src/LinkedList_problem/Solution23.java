ba55ea71f7d17220c05df281fe479da138849a3c
quchao161 <quchao161@pingan.com.cn>
2020-11-22 10:41:16 +0800

    daily study

---
+++ data_struct_study/src/LinkedList_problem/Solution23.java
@@ -0,0 +1,70 @@
+package LinkedList_problem;
+
+import java.util.PriorityQueue;
+
+/**
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ *
+ * 优先队列：用容量为K的最小堆优先队列，把链表的头结点都放进去，然后出队当前
+ * 优先队列中最小的，挂上链表，，然后让出队的那个节点的下一个入队，
+ * 再出队当前优先队列中最小的，直到优先队列为空。
+ * 时间复杂度:O(n*log(k))，n是所有链表中元素的总和，k是链表个数。
+ *
+ * 分支法：分而治之，链表两两合并。时间复杂度:O(n*log(k))
+ */
+public class Solution23 {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((o1, o2) -> o1.val - o2.val);
+
+        for (ListNode list : lists) {
+            if (list == null) {
+                continue;
+            }
+            pq.add(list);
+        }
+
+        while (!pq.isEmpty()) {
+            ListNode nextNode = pq.poll();
+            curr.next = nextNode;
+            curr = curr.next;
+            if (nextNode.next != null) {
+                pq.add(nextNode.next);
+            }
+        }
+        return dummyHead.next;
+    }
+
+    public ListNode mergeKLists2(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    private ListNode merge(ListNode[] lists, int left, int right) {
+        if (left == right) return lists[left];
+        int mid = left + (right - left) / 2;
+        ListNode l1 = merge(lists, left, mid);
+        ListNode l2 = merge(lists, mid + 1, right);
+        return mergeTwoLists(l1, l2);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+
+}
--- data_struct_study/src/LinkedList_problem/Solution25.java
+++ data_struct_study/src/LinkedList_problem/Solution25.java
@@ -2,7 +2,85 @@ package LinkedList_problem;
 
 
 /**
- * 不仅仅是穿针引线
+ * K 个一组翻转链表：不仅仅是穿针引线
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺
+ *
+ * 1、链表分区为已翻转部分+待翻转部分+未翻转部分
+ * 2、每次翻转前，要确定翻转链表的范围，这个必须通过 k 此循环来确定
+ * 3、需记录翻转链表前驱和后继，方便翻转完成后把已翻转部分和未翻转部分连接起来
+ * 4、初始需要两个变量 pre 和 end，pre 代表待翻转链表的前驱，end 代表待翻转链表的末尾
+ * 5、经过k此循环，end 到达末尾，记录待翻转链表的后继 next = end.next
+ * 6、翻转链表，然后将三部分链表连接起来，然后重置 pre 和 end 指针，然后进入下一次循环
+ * 7、特殊情况，当翻转部分长度不足 k 时，在定位 end 完成后，end==null，已经到达末尾，说明题目已完成，直接返回即可
+ * 8、时间复杂度为 O(n*K)最好的情况为 O(n)O 最差的情况未 O(n^2)
+ * 9、空间复杂度为 O(1)除了几个必须的节点指针外，我们并没有占用其他空间
  */
 public class Solution25 {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        //定义一个假的节点。
+        ListNode dummy=new ListNode(0);
+        //假节点的next指向head。
+        // dummy->1->2->3->4->5
+        dummy.next=head;
+        //初始化pre和end都指向dummy。pre指每次要翻转的链表的头结点的上一个节点。end指每次要翻转的链表的尾节点
+        ListNode pre=dummy;
+        ListNode end=dummy;
+
+        while(end.next!=null){
+            //循环k次，找到需要翻转的链表的结尾,这里每次循环要判断end是否等于空,因为如果为空，end.next会报空指针异常。
+            //dummy->1->2->3->4->5 若k为2，循环2次，end指向2
+            for(int i=0;i<k&&end != null;i++){
+                end=end.next;
+            }
+            //如果end==null，即需要翻转的链表的节点数小于k，不执行翻转。
+            if(end==null){
+                break;
+            }
+            //先记录下end.next,方便后面链接链表
+            ListNode next=end.next;
+            //然后断开链表
+            end.next=null;
+            //记录下要翻转链表的头节点
+            ListNode start=pre.next;
+            //翻转链表,pre.next指向翻转后的链表。1->2 变成2->1。 dummy->2->1
+            pre.next=reverse(start);
+            //翻转后头节点变到最后。通过.next把断开的链表重新链接。
+            start.next=next;
+            //将pre换成下次要翻转的链表的头结点的上一个节点。即start
+            pre=start;
+            //翻转结束，将end置为下次要翻转的链表的头结点的上一个节点。即start
+            end=start;
+        }
+        return dummy.next;
+
+
+    }
+
+    //链表翻转
+    // 例子：   head： 1->2->3->4
+    public ListNode reverse(ListNode head) {
+        //单链表为空或只有一个节点，直接返回原单链表
+        if (head == null || head.next == null){
+            return head;
+        }
+        //前一个节点指针
+        ListNode preNode = null;
+        //当前节点指针
+        ListNode curNode = head;
+        //下一个节点指针
+        ListNode nextNode = null;
+        while (curNode != null){
+            nextNode = curNode.next;//nextNode 指向下一个节点,保存当前节点后面的链表。
+            curNode.next=preNode;//将当前节点next域指向前一个节点   null<-1<-2<-3<-4
+            preNode = curNode;//preNode 指针向后移动。preNode指向当前节点。
+            curNode = nextNode;//curNode指针向后移动。下一个节点变成当前节点
+        }
+        return preNode;
+    }
 }
